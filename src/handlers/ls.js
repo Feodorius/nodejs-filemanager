473bda1fa22dc60d6e035aca276746c88aeea4b0
Fedor Kaputsky <fedor.kaputsky@sap.com>
2025-05-04 22:13:16 +0300

    fix: add more types for Dirent and sort them

--- src/handlers/ls.js
+++ src/handlers/ls.js
@@ -8,17 +8,40 @@ const ls = async () => {
     const table = files.map(file => {
         return {
             name: file.name,
-            type: file.isDirectory() ? "directory" : "file"
+            type: getFileType(file)
         }
     });
     const sortedTable = table.toSorted(
         (a, b) => {
-            if (a.type === b.type) {
-              return a.name.localeCompare(b.name);
+            if (a.type === "directory" && b.type !== "directory") {
+                return -1;
             }
-            return a.type === 'directory' ? -1 : 1;
-          }
+            if (a.type !== "directory" && b.type === "directory") {
+                return 1;
+            }
+            return a.type.localeCompare(b.type);
+        }
     );
     console.table(sortedTable);
 }
 export default ls;
+
+function getFileType(dirent) {
+    if (dirent.isFile()) {
+        return "file";
+    } else if (dirent.isDirectory()) {
+        return "directory";
+    } else if (dirent.isSymbolicLink()) {
+        return "symbolic link";
+    } else if (dirent.isBlockDevice()) {
+        return "block device";
+    } else if (dirent.isCharacterDevice()) {
+        return "character device";
+    } else if (dirent.isFIFO()) {
+        return "FIFO";
+    } else if (dirent.isSocket()) {
+        return "socket";
+    } else {
+        return "unknown";
+    }
+}
