dbc0cc9bdcee5d98b742d94a3a326dd670faa6cf
Fedor Kaputsky <fedor.kaputsky@sap.com>
2025-05-05 13:51:12 +0300

    feat: add compress/decompress functions

--- src/handlers/handlersMap.js
+++ src/handlers/handlersMap.js
@@ -2,7 +2,9 @@ import exitProgram from "../utils/exitProgram.js";
 import add from "./add.js";
 import cat from "./cat.js";
 import cd from "./cd.js";
+import compress from "./compress.js";
 import cp from "./cp.js";
+import decompress from "./decompress.js";
 import ls from "./ls.js";
 import mkdir from "./mkdir.js";
 import mv from "./mv.js";
@@ -25,8 +27,8 @@ const handlersMap = new Map(Object.entries({
     "rm": rm,
     "os": os,
     // "hash": hash,
-    // "compress": compress,
-    // "decompress": decompress
+    "compress": compress,
+    "decompress": decompress
 }));
 
 export default handlersMap;
